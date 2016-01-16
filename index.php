719c01b72b2533ba5f3631b9fc957e815610129a
yashted <yashted@gmail.com>
2016-01-16 10:07:22 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Yashaswi L P</title>
+	<link href='http://fonts.googleapis.com/css?family=Kenia' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+	
+	<div class="container-main">
+		<div class="container-header">
+			<div class="container-logo"> 
+				<h1>yashaswi prakash</h1>
+			</div>
+			<div class="icon-container">
+				<div class="icon-container-actual">
+					<a href="#" class="icon-facebook" title="Facebook"></a>
+					<a href="#" class="icon-twitter" title="Twitter"></a>
+					<a href="#" class="icon-google" title="Google"></a>
+					<a href="#" class="icon-linkedin" title="Linkedin"></a>
+				</div>
+			</div>
+			<div class="container-body-menu">
+				<div class="container-body-menu-top">
+				</div>
+				<div class="container-body-menu-bottom">
+				</div>
+			</div>
+		</div>
+		<div class="container-menu">
+			<div class="button-container">
+				<a href="#" class="button">Home</a>
+				<a href="#" class="button">About Me</a>
+				<a href="#" class="button">Portfolio</a>
+				<a href="#" class="button">Gallery</a>
+				<a href="#" class="button">Contact Me</a>
+			</div>
+		</div>
+		<div class="container-body"> 			
+			<div class="container-body-main">
+				<!-- POST -->
+				<div class="post-container">
+					
+					<div class="post-container-header">
+						 <div id="container-ribbon">  
+					        <div class="container-ribbon-div"> 
+					        	<h3>My Heading My heading my heading my heading my </h3>
+					        	<div class="container-ribbon-postcount">
+					        		<h4>222</h4>
+					        	</div>
+					        	<span class="arrow"></span> 
+					       	</div>  
+					    </div>
+						<div class="container-header-postdetails">
+							<p>Posted by: admin &sect; on April 23rd 2011</p>
+						</div>						    
+					</div> 
+					<div class="post-container-body">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vestibulum libero vel orci blandit hendrerit. In dignissim nunc venenatis sapien facilisis tincidunt elementum est imperdiet. Nullam augue dui, vehicula ut facilisis nec, condimentum eget sapien. In ut accumsan ante. In adipiscing enim nulla, ac posuere massa. Etiam sit amet enim id nunc vestibulum malesuada ut vel dui. Cras eu odio augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent quam nisl, placerat sit amet luctus vitae, interdum sit amet neque. Etiam vel faucibus turpis. Proin pulvinar sollicitudin justo, in lacinia velit varius eget. Quisque bibendum, massa sit amet aliquam consequat, ligula quam condimentum diam, vel tincidunt erat eros eget justo. Integer at dui non metus iaculis pharetra. Nullam non condimentum dolor. Maecenas placerat aliquam dui id bibendum. Nullam eu lacus a dui ultricies convallis. In hac habitasse platea dictumst. Maecenas tincidunt lectus eu turpis convallis eu imperdiet lorem posuere. Donec rhoncus erat vitae lorem molestie vestibulum. Vestibulum mattis sagittis neque ac venenatis.</p>
+						{ <a href="#" class="posts-more">Read More...</a> }
+					</div>
+				</div>
+				<!-- POST -->
+				<!-- POST .. -->
+				<div class="post-container">
+					
+					<div class="post-container-header">
+						 <div id="container-ribbon">  
+					        <div class="container-ribbon-div"> 
+					        	<h3>My Heading My heading my heading my heading my </h3>
+					        	<div class="container-ribbon-postcount">
+					        		<h4>222</h4>
+					        	</div>
+					        	<span class="arrow"></span> 
+					       	</div>  
+					    </div>
+						<div class="container-header-postdetails">
+							<p>Posted by: admin &sect; on April 23rd 2011</p>
+						</div>						    
+					</div> 
+					<div class="post-container-body">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vestibulum libero vel orci blandit hendrerit. In dignissim nunc venenatis sapien facilisis tincidunt elementum est imperdiet. Nullam augue dui, vehicula ut facilisis nec, condimentum eget sapien. In ut accumsan ante. In adipiscing enim nulla, ac posuere massa. Etiam sit amet enim id nunc vestibulum malesuada ut vel dui. Cras eu odio augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent quam nisl, placerat sit amet luctus vitae, interdum sit amet neque. Etiam vel faucibus turpis. Proin pulvinar sollicitudin justo, in lacinia velit varius eget. Quisque bibendum, massa sit amet aliquam consequat, ligula quam condimentum diam, vel tincidunt erat eros eget justo. Integer at dui non metus iaculis pharetra. Nullam non condimentum dolor. Maecenas placerat aliquam dui id bibendum. Nullam eu lacus a dui ultricies convallis. In hac habitasse platea dictumst. Maecenas tincidunt lectus eu turpis convallis eu imperdiet lorem posuere. Donec rhoncus erat vitae lorem molestie vestibulum. Vestibulum mattis sagittis neque ac venenatis.</p>
+						{ <a href="#" class="posts-more">Read More...</a> }
+					</div>
+				</div>
+				<!-- POST -->
+				
+			</div>
+			<div class="container-body-sidebar">
+				<div class="container-body-sidebar-image-outer">
+					<div class="container-body-sidebar-image">
+						<img src="snapshot_Cyanotype.jpg" alt="" />
+					</div>
+				</div>
+				<div class="container-body-sidebar-about-text">
+					<p>Yashaswi Prakash</p>
+					<p>Mysore</p>
+					<p>Student at </p><p>Sri Jayachamarajendra </p><p>College Of</p><p>Engineering(MCA)</p>
+				</div>
+			</div>
+		</div>
+		<div class="container-footer">
+			<div class="container-footer-top">
+			</div>
+			<div class="container-footer-bottom">
+				<p>Built using</p>
+				<img src="icons/inFocus Social Media Icons/Icons/grey/wordpress.png" alt="WordPress"/>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
